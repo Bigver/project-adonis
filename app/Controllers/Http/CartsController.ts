7fe5afdb160defea108f885310ed3c79756112d7
pahs2554 <tanaphatw64@nu.ac.th>
2024-05-03 16:37:41 +0700

    Function Cart By PETCH

---
+++ app/Controllers/Http/CartsController.ts
@@ -0,0 +1,35 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CartService from 'App/Service/CartService';
+
+export default class CartsController {
+
+    async addCart({ request, response }: HttpContextContract) {
+        try {
+            const userId = request.input('userId');
+            let cart;
+
+
+            const existingCart = await CartService.findCartByUserId(userId);
+
+            if (!existingCart) {
+                const data = { userId };
+                cart = await CartService.create(data);
+            } else {
+                cart = existingCart;
+            }
+
+            const detail = {
+                cartId: cart.id,
+                productId: request.input('productId'),
+                quantity: request.input('quantity')
+            };
+
+            await CartService.createDetail(detail);
+            return response.redirect().back()
+        } catch (error) {
+            console.error(error);
+            return response.status(500).json({ error: 'Failed to add Cart detail' });
+        }
+    }
+
+}
