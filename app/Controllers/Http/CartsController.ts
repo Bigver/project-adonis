2d55e52be10639b3e98bf18f8bdef69c266706ed
Sirawitch <sirawitchbu@kkumail.com>
2024-05-15 18:02:16 +0700

    shopcart page add more modal

--- app/Controllers/Http/CartsController.ts
+++ app/Controllers/Http/CartsController.ts
@@ -1,45 +1,104 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import CartService from 'App/Service/CartService';
+import CartItemsService from 'App/Service/cart_item_service';
+import CartService from 'App/Service/cart_service';
+import LogService from 'App/Service/log_service';
+import ProductService from 'App/Service/product_service';
 
 export default class CartsController {
 
-    async addCart({ request, response }: HttpContextContract) {
+    async shopCart({ view, auth }: HttpContextContract) {
+        try {
+            const user = auth.user?.serialize();
+            const filter = {};
+            if (!user) {
+                return view.render('errors.unauthorized');
+            }
+            const carts = await CartItemsService.getCartByUser(user.id) //Items
+            const cartItem = await CartService.findByUserId(user.id) //Cart
+            const item = await ProductService.all({ filter: filter })
+            const items = item.serialize()
+
+            const serializedCartItems = carts.map((item) => item.serialize());
+            let total = 0;
+            serializedCartItems.forEach((item: any) => {
+                total += item.total_price;
+            });
+
+            return view.render("user/shopcartPage", { user: user, items, cartData: carts, total, cartItem });
+        } catch (error) {
+            const { level, message, context } = {
+                level: "warn",
+                message: "Failed to open Shop Cart page",
+                context: {
+                    userId: auth.user?.id
+                }
+            };
+            await LogService.create(level, message, context);
+            error = "Failed to open Shop Cart page"
+            return view.render('error', { error })
+        }
+    }
+
+    async deleteCart({ response, params }: HttpContextContract) {
+        await CartItemsService.delete(params.id)
+        return response.redirect().back()
+    }
+
+
+    async addCart({ request, response, view }: HttpContextContract) {
         try {
             const userId = request.input('userId');
             let cart;
-
-
-            const existingCart = await CartService.findCartByUserId(userId);
-
-            if (!existingCart) {
+            const cartData = await CartService.findByUserId(userId);
+            if (!cartData) {
                 const data = { userId };
                 cart = await CartService.create(data);
             } else {
-                cart = existingCart;
+                cart = cartData;
             }
-
             const detail = {
                 cartId: cart.id,
                 productId: request.input('productId'),
                 quantity: request.input('quantity')
             };
-
-            await CartService.createDetail(detail);
+            await CartItemsService.create(detail);
             return response.redirect().back()
         } catch (error) {
-            console.error(error);
-            return response.status(500).json({ error: 'Failed to add Cart detail' });
+            const { level, message, context } = {
+                level: "warn",
+                message: "Add cart failed",
+                context: {
+                    userId: request.input('userId'),
+                    productId: request.input('productId'),
+                    quantity: request.input('quantity')
+                }
+            };
+            await LogService.create(level, message, context);
+            error = "add cart failed"
+            return view.render('error', { error })
+        }
+    }
+
+    async increaseProductQuantityInCart({ view, response, params }: HttpContextContract) {
+        try {
+            await CartItemsService.increaseProduct(params.id)
+            return response.redirect().back()
+        } catch (error) {
+            const { level, message, context } = {
+                level: "warn",
+                message: "failed to increse product quantity",
+                context: {
+                    params: params.id
+                }
+            }
+            await LogService.create(level, message, context);
+            error = "failed to increse product quantity"
+            return view.render('error', { error })
         }
     }
 
     async decreaseProductQuantityInCart({response,params} : HttpContextContract) {
-        await CartService.decreaseProduct(params.id)
+        await CartItemsService.decreaseProduct(params.id)
         return response.redirect().back()
     }
-
-    async increaseProductQuantityInCart({response,params} : HttpContextContract) {
-        await CartService.increaseProduct(params.id)
-        return response.redirect().back()
-    }
-
 }
---
+++ app/Service/cart_item_service.ts
@@ -0,0 +1,61 @@
+import CartItem from "App/Models/CartItem"
+import _ from 'lodash'
+
+export default class CartItemsService {
+    public static getItemById(filters?: any) { // 
+        let query = CartItem.query();
+        if (filters && filters.id) {
+            query = query.where('id', '=', filters.id);
+        }
+        return query;
+    }
+
+    public static async getCartByUser(userId: number) { // Cart Items
+        const cartItem = await CartItem.query()
+            .whereHas('cart', (query) => {
+                query.where('userId', userId);
+            })
+            .preload('product');
+        return cartItem;
+    }
+
+    public static async decreaseProduct(id: any) {
+        const item = await CartItem.findOrFail(id);
+        if (item.quantity > 1) {
+            item.quantity = item.quantity - 1;
+            await item.save();
+            return item;
+        } else {
+            await item.delete();
+            return null;
+        }
+    }
+
+    public static async create(data: any) {
+        const item = await CartItem.query()
+            .where('cartId', data.cartId)
+            .where('productId', data.productId)
+            .first();
+
+        if (item) {
+            item.quantity += parseInt(data.quantity);
+            item.totalPrice += parseInt(data.totalPrice);
+            await item.save();
+        } else {
+            const item = await CartItem.create(data);
+            return item;
+        }
+    }
+
+    public static async increaseProduct(id: any) {
+        const item = await CartItem.findOrFail(id);
+        item.quantity = item.quantity + 1;
+        await item.save();
+        return item;
+    }
+
+    static async delete(id: any) {
+        const item = await CartItem.findOrFail(id)
+        return await item.delete()
+    }
+}
